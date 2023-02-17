f02c119cbae4154f82edde09c09894f27525195c
Anovius <anovius10@gmail.com>
2023-02-17 17:43:31 +0500

    login with token

--- app/controllers/api/v1/session_controller.rb
+++ app/controllers/api/v1/session_controller.rb
@@ -2,14 +2,9 @@ class Api::V1::SessionController < ApplicationController
 
     def create
         user = User.where(email: params[:email]).first
-
-        # console log user to see what it returns
-
-        puts "prarams: #{params[:email]}"
-
         if user
             if user.valid_password?(params[:password])
-                render json: user.as_json(only: [:id, :email]), status: :created
+                render json: user.toAuthJSON, status: :created
             else
                 render json: { errors: "Invalid password" }, status: :unauthorized
             end
